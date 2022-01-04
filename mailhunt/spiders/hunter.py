81a8a8fca8bec4c73c041955b0234beb765766f7
p371k9 <petik2peter@gmail.com>
2022-01-04 19:03:34 +0100

    no remove tags, better crawling

--- mailhunt/spiders/hunter.py
+++ mailhunt/spiders/hunter.py
@@ -4,7 +4,7 @@ from scrapy.http import Request
 import re
 from mailhunt.items import MailhuntItem
 from urllib.parse import urlparse
-from w3lib.html import remove_tags
+#from w3lib.html import remove_tags
 from html import unescape
 #from scrapy.shell import inspect_response
 
@@ -14,8 +14,7 @@ class MSpider(scrapy.Spider):
     
     def __init__(self, url=None, list=None, *args, **kwargs):        
         if url != None:
-            self.start_urls = [url]
-            #self.start_urls = getUrls(url)
+            self.start_urls = [url]            
         if list != None:
             with open(list) as f:
                 content = f.readlines()
@@ -28,14 +27,15 @@ class MSpider(scrapy.Spider):
 
     def parse(self, response):
         def search_in_pg():
-            match = re.search(r'[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}', remove_tags(unescape(response.text)).strip())
+            #inspect_response(response, self) 
+            match = re.search(r'[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}', unescape(response.text).replace('<em>', ''))
             if (match):
                 self.logger.info('************************')
                 return match.group()
             else:
                 return ''
 
-        #inspect_response(response, self)               
+                      
         mail = search_in_pg()
         if mail != '':
             doc = MailhuntItem()            
@@ -51,12 +51,14 @@ class MSpider(scrapy.Spider):
                 yield response.follow(url=nl, meta={'start_url': response.meta['start_url'], 'links': response.meta['links']}, callback=self.parse)                 
         else:
             links = []
-            for x in response.xpath('//a/@href'):            
-                href = x.extract()
-                href = href.replace('\\','/')            
-                for y in self.settings.get('RLS'): 
+            for y in self.settings.get('RLS'): 
+                for x in response.xpath('//a/@href'):            
+                    href = x.extract()
+                    href = href.replace('\\','/')            
+            
                     if y in href:
                         links.append(href)
+
             if len(links) > 0:
                 nl = links[0]
                 links.pop(0)
